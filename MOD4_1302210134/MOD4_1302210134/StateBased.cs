3970e6c2c14badbb128632d5ea63fcbe6b880ad5
chandra575 <67907359+chandra575@users.noreply.github.com>
2023-03-12 20:58:26 +0700

    inisialisasi projek

---
+++ MOD4_1302210134/MOD4_1302210134/StateBased.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class DoorMachine
+{
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = new LockedState();
+    }
+
+    public void SetState(State state)
+    {
+        currentState = state;
+        currentState.DisplayState();
+    }
+}
+
+interface State
+{
+    void DisplayState();
+}
+
+class LockedState : State
+{
+    public void DisplayState()
+    {
+        Console.WriteLine("Pintu terkunci");
+    }
+}
+
+class UnlockedState : State
+{
+    public void DisplayState()
+    {
+        Console.WriteLine("Pintu tidak terkunci");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DoorMachine doorMachine = new DoorMachine();
+        doorMachine.SetState(new UnlockedState());
+        doorMachine.SetState(new LockedState());
+    }
+}
---
+++ MOD4_1302210134/MOD4_1302210134/TableDriven.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class KodePos
+{
+    private Dictionary<string, string> tabelKodePos = new Dictionary<string, string>
+    {
+        {"Batununggal", "40266"},
+        {"A. Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
+    };
+
+    public string getKodePos(string kelurahan)
+    {
+        if (tabelKodePos.ContainsKey(kelurahan))
+        {
+            return tabelKodePos[kelurahan];
+        }
+        else
+        {
+            return "Kode pos tidak ditemukan.";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kodepos = new KodePos();
+
+        Console.WriteLine(kodepos.getKodePos("Batununggal"));
+        Console.WriteLine(kodepos.getKodePos("A. Kujangsari"));
+        Console.WriteLine(kodepos.getKodePos("Mengger"));
+        Console.WriteLine(kodepos.getKodePos("Wates"));
+        Console.WriteLine(kodepos.getKodePos("Cijaura"));
+        Console.WriteLine(kodepos.getKodePos("Jatisari"));
+        Console.WriteLine(kodepos.getKodePos("Margasari"));
+        Console.WriteLine(kodepos.getKodePos("Sekejati"));
+        Console.WriteLine(kodepos.getKodePos("Kebonwaru"));
+        Console.WriteLine(kodepos.getKodePos("Maleer"));
+        Console.WriteLine(kodepos.getKodePos("Samoja"));
+        Console.WriteLine(kodepos.getKodePos("Sukabirus"));
+    }
+}
+
